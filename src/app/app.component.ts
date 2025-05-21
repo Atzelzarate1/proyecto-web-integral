e7846a488ecd1b6681d4ec0fe8eae869735bdc05
atzel <atzelzarate09@gmail.com>
2025-05-21 10:00:51 -0600

    app.html y ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'practica_4';
+  constructor(){
+    console.log("nuevas funciones");
+    console.warn("cuidado")
+    console.log("prueba de conflictos");
+    
+  }
 }
