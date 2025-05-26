3d0fde459385e82908723704f9555778c064f56d
atzel <atzelzarate09@gmail.com>
2025-05-26 09:55:58 -0600

    diseño register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
      { path: 'auth', component: AuthComponent },
   { path: '', redirectTo: '/auth', pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent },
 ];
